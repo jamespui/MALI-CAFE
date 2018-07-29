dbf6383dd0a31ccb5290af882f20b5cd499f1e96
LaoHan <jimocheng97@hotmail.com>
2018-07-29 23:10:24 +0800

    remove unuse code

--- malicafe-website/route/route.js
+++ malicafe-website/route/route.js
@@ -3,15 +3,7 @@ const app = new express.Router();
 
 // Serve link example (http://localhost:3000/__)
 app.get('/', function(req, res) {
-    res.render('index', {title: 'start-node', message: 'Hello there!'});
-});
-
-app.get('/playground/angularjs', function(req, res) {
-    res.render('playground/angularjs', {title: 'Welcome to AngularJS', message: 'Hello there!'});
-});
-
-app.get('/playground/vuejs', function(req, res) {
-    res.render('playground/vuejs', {title: 'Welcome to VueJS', message: 'Hello there!'});
+    res.render('index', {title: 'Malicafe', message: 'Hello there!'});
 });
 
 app.get('/signup', function(req, res) {
