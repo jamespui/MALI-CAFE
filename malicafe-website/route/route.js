3fef2bc4aec3e217346503b9019d3eef7b037754
LaoHan <jimocheng97@hotmail.com>
2018-08-04 00:02:19 +0800

    wtf image file szie so big

--- malicafe-website/route/route.js
+++ malicafe-website/route/route.js
@@ -6,8 +6,12 @@ app.get('/', function(req, res) {
     res.render('home', {title: 'Malicafe', message: 'Hello there!'});
 });
 
-app.get('/signup', function(req, res) {
-    res.render('account/signup', {title: 'Welcome to start-node', message: 'Hello there!'});
+app.get('/register', function(req, res) {
+    res.render('account/signup', {title: 'Register', message: 'Hello there!'});
+});
+
+app.get('/login', function(req, res) {
+    res.render('account/signin', {title: 'Login', message: 'Hello there!'});
 });
 
 module.exports = app;
