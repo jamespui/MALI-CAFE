9ed586d2a7d0eb85a430f44f9ec38cfbc0d43398
James Pui <jkspui@gmail.com>
2018-08-05 15:20:07 +0800

    (MALICAFE-james) 我的register&login做好了不能够link ，我做法跟你的一样了还是不会link，是custom.js控制我的link吗？我找不到custom.js里是哪个控制link。 。很像被锁这样 lol

---
+++ malicafe-laohan/route/route.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = new express.Router();
+
+// Serve link example (http://localhost:3000/__)
+app.get('/home', function(req, res) {
+    res.render('home', {title: 'Malicafe', message: 'Hello there!'});
+});
+
+app.get('/register', function(req, res) {
+    res.render('account/signup', {title: 'Register', message: 'Hello there!'});
+});
+
+app.get('/login', function(req, res) {
+    res.render('account/signin', {title: 'Login', message: 'Hello there!'});
+});
+
+module.exports = app;
